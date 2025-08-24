5983569768e31363ba2718a4a756674da310f541
Kushaal Challani <66863823+kushaalchallani@users.noreply.github.com>
2025-08-24 18:19:51 +0530

    [Fixed] Crafts Multiple Items with insufficient materials

--- Assets/Scripts/CraftingSystem.cs
+++ Assets/Scripts/CraftingSystem.cs
@@ -67,7 +67,7 @@ public class CraftingSystem : MonoBehaviour {
 
 
     public IEnumerator calculate() {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.1f);
         InventorySystem.Instance.ReCalculateList();
     }
 
